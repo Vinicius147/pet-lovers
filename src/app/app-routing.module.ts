d5736905c50ff81a4f294ced06a8dd26d69395ab
Vinicius Godinho <viniciusgodinho2009@hotmail.com>
2023-06-13 01:42:09 -0300

    criado rota para a pagina end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { QueroAdotarComponent } from './quero-adotar/quero-adotar.component';
 import { IndexComponent } from './index/index.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { EndComponent } from './end/end.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full'},
   { path: 'index', component: IndexComponent},
   { path: 'quero-adotar', component: QueroAdotarComponent},
-  { path: 'forms', component: FormularioComponent}
+  { path: 'forms', component: FormularioComponent},
+  { path: 'end', component: EndComponent}
 ];
 
 @NgModule({
